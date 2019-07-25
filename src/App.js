4d893d01abc617ae41c346aef8f0bc82a3d75a02
jtaaa <joshuatallum@gmail.com>
2019-07-25 17:14:42 -0400

    Update to accept evaluation results

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import EvaluationForm from './components/forms/EvaluationForm';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-content">
-        <header><h1>iPhones TT</h1></header>
-        <div className="App-form">
-          <EvaluationForm />
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      evaluationResults: null, 
+    };
+  }
+
+  handleEvaluationSubmit = (results) => {
+    this.setState({ evaluationResults: results });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-content">
+          <header><h1>iPhones TT</h1></header>
+          <div className="App-form">
+            { !this.state.evaluationResults &&
+            <EvaluationForm handleSubmit={this.handleEvaluationSubmit} /> }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
