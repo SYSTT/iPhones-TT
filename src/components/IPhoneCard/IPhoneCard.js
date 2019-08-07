51e3cae362fb8d01c92d2d3cb1d44d596a0fc259
jtaaa <joshuatallum@gmail.com>
2019-08-07 00:56:42 -0400

    Only display button if link is provided

--- src/components/IPhoneCard/IPhoneCard.js
+++ src/components/IPhoneCard/IPhoneCard.js
@@ -4,14 +4,16 @@ import './IPhoneCard.css';
 
 import IPhone from './IPhone.svg';
 
-function IPhoneCard({ title, description, buttonText, color, buttonColor, link }) {
+function IPhoneCard({ title, description, color, buttonText, buttonColor, link }) {
     return (
         <div className="IPhoneCard" style={{ backgroundColor: color }}>
             <h3 className="IPhoneCard-title">{ title }</h3>
             <div className="IPhoneCard-split">
                 <div className="IPhoneCard-content">
                     <div className="IPhoneCard-description">{ description }</div>
+                    { link &&
                     <Link to={link} className="IPhoneCard-button" style={{ backgroundColor: buttonColor }}>{ buttonText }</Link>
+                    }
                 </div>
                 <img src={IPhone} alt="iPhone" />
             </div>
