1ea58ccc42d8455a65cb4df909707d62461560d4
jtaaa <joshuatallum@gmail.com>
2019-08-07 00:39:57 -0400

    Add initial IPhoneCard

---
+++ src/components/IPhoneCard/IPhoneCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './IPhoneCard.css';
+
+import IPhone from './IPhone.svg';
+
+function IPhoneCard({ title, description, buttonText, color, buttonColor, link }) {
+    return (
+        <div className="IPhoneCard" style={{ backgroundColor: color }}>
+            <h3 className="IPhoneCard-title">{ title }</h3>
+            <div className="IPhoneCard-split">
+                <div className="IPhoneCard-content">
+                    <div className="IPhoneCard-description">{ description }</div>
+                    <Link to={link} className="IPhoneCard-button" style={{ backgroundColor: buttonColor }}>{ buttonText }</Link>
+                </div>
+                <img src={IPhone} alt="iPhone" />
+            </div>
+        </div>
+    );
+}
+
+export default IPhoneCard;
