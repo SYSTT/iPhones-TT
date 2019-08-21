c3b376720dbbe68a7713091f643623d9c9c023ce
jtaaa <joshuatallum@gmail.com>
2019-08-21 00:29:11 -0400

    Add selection to buy page

--- src/components/BuyPage/BuyPage.js
+++ src/components/BuyPage/BuyPage.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './BuyPage.css';
 
-function BuyPage() {
+function BuyPage({ location }) {
     const [priceTable, setPriceTable] = useState(null);
+    const [selected, setSelected] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(null);
 
     useEffect(() => {
         async function fetchPriceTable() {
@@ -13,23 +16,64 @@ function BuyPage() {
         fetchPriceTable();
     }, []);
 
-    const items = priceTable ? Object.keys(priceTable).map(model => (
-        <div className="BuyPage-item" key={model}>
-            <h3 className="BuyPage-item-title">{ priceTable[model].name }</h3>
+    useEffect(() => {
+        if (location.search) {
+            const params = location.search.slice(1).split('&');
+            const paramsObj = {};
+            for (const param of params) {
+                const [key, val] = param.split('=');
+                paramsObj[key] = val;
+            }
+            setSelected(paramsObj);
+        } else {
+            setSelected(null);
+        }
+    }, [location.search])
+
+    const makeSelection = (index, model, memory) => {
+        setSelected({ model, memory });
+        setSelectedIndex(index);
+
+    }
+
+    const modelOption = (model, memory) => (priceTable ?
+        <Link
+            key={memory}
+            to={`?model=${model}&memory=${memory}`}
+            className="BuyPage-item-option"
+        >
+            <div className="BuyPage-item-memory">{ memory.toUpperCase() }</div>
+            <div className="BuyPage-item-price">${ priceTable[model].prices[memory] }</div>
+        </Link>
+        : null
+    );
+
+    const item = (model, memory) => (priceTable ? 
+        <div
+            key={model}
+            className={`BuyPage-item ${selected ? 'BuyPage-item-selected' : null}`}
+        >
+            <h3
+                className={`BuyPage-item-title ${selected ? 'BuyPage-item-title-selected' : null}`}
+            >
+                { priceTable[model].name }
+            </h3>
             <div className="BuyPage-item-options">
-                { Object.keys(priceTable[model].prices).map(memory => (
-                <div className="BuyPage-item-option" key={memory}>
-                    <div className="BuyPage-item-memory">{ memory.toUpperCase() }</div>
-                    <div className="BuyPage-item-price">${ priceTable[model].prices[memory] }</div>
-                </div>
-                ))}
+                { !memory
+                ?   Object.keys(priceTable[model].prices).map(memory => modelOption(model, memory))
+                :   modelOption(model, memory)}
             </div>
         </div>
+        : null
+    );
+
+    const items = priceTable ? Object.keys(priceTable).map((model, index) => (
+        item(model)
     ))
     : null;
-    
-    return (
-        <div className="BuyPage">
+
+    const preselection = (
+        <>
             <h1>Purchase iPhones</h1>
             <p>Find the list of iPhones available and prices below.</p>
             <div className="BuyPage-list">
@@ -41,6 +85,18 @@ function BuyPage() {
                     <button type="submit">Contact</button>
                 </a>
             </div>
+        </>
+    );
+
+    const postSelection = selected ? (
+        <>
+            { item(selected.model, selected.memory) }
+        </>
+    ) : null;
+    
+    return (
+        <div className="BuyPage">
+            { !selected ? preselection : postSelection }
         </div>
     );
 }
