367e0fd3f23b27295bb56b4d6d0b38bc0b25f727
jtaaa <joshuatallum@gmail.com>
2019-08-22 23:29:55 -0400

    Update buy page copy

--- src/components/BuyPage/BuyPage.js
+++ src/components/BuyPage/BuyPage.js
@@ -71,7 +71,7 @@ function BuyPage({ location, history }) {
     const preselection = (
         <div className="BuyPage-content">
             <h1>Buy Your iPhone Here</h1>
-            <p>View our available iPhones and prices below. Prices are for A-Grade iPhones, configurations with * are for brand new devices.</p>
+            <p>View our available iPhones and prices below. We’ve marked New iPhones with an “*” and left A-Grade iPhones unmarked.</p>
             <div className="BuyPage-list">
                 {items}
             </div>
