2fb1e62739c5df0edd4377ad26bb7edc37d2696d
jtaaa <joshuatallum@gmail.com>
2019-09-07 17:09:12 -0400

    Add extra options to button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Button.css';
 
-function Button({ text, onClick }) {
+function Button({ text, onClick, color, backgroundColor, fitted, marginRight }) {
+    let className = "Button";
+    if (fitted) { className += " Button-fitted"; }
     return (
         <input
             type="button"
-            className="Button"
+            className={className}
             onClick={onClick}
             value={text}
+            style={{ color, backgroundColor, marginRight: marginRight ? '1em' : null }}
         />
     );
 }
