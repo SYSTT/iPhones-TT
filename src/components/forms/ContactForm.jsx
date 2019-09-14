fa0c04ccb8aa6b5c5d389bdefdcacf5d2c50ad54
jtaaa <joshuatallum@gmail.com>
2019-09-14 02:26:58 -0400

    Update contact form for embeded use

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -1,77 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ContactForm.css';
 
-function ContactForm({ onSubmit }) {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [tel, setTel] = useState('');
-    const [address, setAddress] = useState('');
+function ContactForm({ contactInfo, setContactInfo }) {
+  const onChange = (key, value) => {
+    setContactInfo({
+      ...contactInfo,
+      [key]: value, 
+    });
+  }
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (name && email && tel && address) {
-        onSubmit({ name, email, tel, address });
-      } else {
-        alert('Please fill in all the contact fields above.')
-      }
-    };
+  const { name, email, tel, address } = contactInfo;
 
-    return (
-        <div className="ContactForm">
-        <h1>Place Your Order Here</h1>
-        <p>Fill your information in below and we’ll call you within the hour to confirm your order.</p>
-        <br/>
-        <form>
-          <div className="Form-fieldset">
-            <div className="Form-field">
-                <label className="Form-label" htmlFor="name"><p>Name:</p></label>
-                <input
-                name="name"
-                id="name"
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                />
-            </div>
-            <div className="Form-field">
-                <label className="Form-label" htmlFor="email"><p>Email Address:</p></label>
-                <input
-                name="email"
-                id="email"
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                />
-            </div>
-          </div>
-          <div className="Form-fieldset">
-            <div className="Form-field">
-              <label className="Form-label" htmlFor="tel"><p>Phone Number:</p></label>
-              <input
-                name="tel"
-                id="tel"
-                type="tel"
-                onChange={(e) => setTel(e.target.value)}
-                value={tel}
-              />
-            </div>
-            <div className="Form-field">
-              <label className="Form-label" htmlFor="address"><p>Address:</p></label>
-              <input
-                name="address"
-                id="address"
-                type="text"
-                onChange={(e) => setAddress(e.target.value)}
-                value={address}
-              />
-            </div>
-          </div>
-          <div className="Form-submit">
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-          </div>
-        </form>
+  return (
+    <form className="ContactForm">
+      <div className="Form-fieldset">
+        <div className="Form-field">
+            <label className="Form-label" htmlFor="name"><p>Name:</p></label>
+            <input
+            name="name"
+            id="name"
+            type="text"
+            onChange={e => onChange('name', e.target.value)}
+            value={name}
+            />
+        </div>
+        <div className="Form-field">
+            <label className="Form-label" htmlFor="email"><p>Email Address:</p></label>
+            <input
+            name="email"
+            id="email"
+            type="email"
+            onChange={e => onChange('email', e.target.value)}
+            value={email}
+            />
+        </div>
       </div>
-    );
+      <div className="Form-fieldset">
+        <div className="Form-field">
+          <label className="Form-label" htmlFor="tel"><p>Phone Number:</p></label>
+          <input
+            name="tel"
+            id="tel"
+            type="tel"
+            onChange={e => onChange('tel', e.target.value)}
+            value={tel}
+          />
+        </div>
+        <div className="Form-field">
+          <label className="Form-label" htmlFor="address"><p>Address:</p></label>
+          <input
+            name="address"
+            id="address"
+            type="text"
+            onChange={e => onChange('address', e.target.value)}
+            value={address}
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
