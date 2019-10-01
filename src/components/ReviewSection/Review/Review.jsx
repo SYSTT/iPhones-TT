1d0b7e8291bae1c3d27266241474753cf38b68db
Ethan <ethan.samuel97@yahoo.com>
2019-10-01 16:53:03 -0400

    Review section

--- src/components/ReviewSection/Review/Review.jsx
+++ src/components/ReviewSection/Review/Review.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Review.css';
 
-function Review({ text, user, timestamp }) {
+function Review(props) {
   return (
     <div className="Review">
+      <p>{props.text}</p>
+      <h3>{props.user}</h3>
+      <p>{props.timestamp}</p>
     </div>
   );
 };
--- src/components/ReviewSection/ReviewSection.jsx
+++ src/components/ReviewSection/ReviewSection.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './ReviewSection.css';
+import Review from "./Review/Review"
+import reviews from "./Review/ReviewData"
 
-function ReviewSection({ reviews }) {
+function ReviewSection() {
+  const userReviews = reviews.map(review => <Review key={review.user.id} text={review.text}
+  user={review.user.name} timestamp={review.timestamp} />)
+
   return (
     <div className="ReviewSection">
+      <h1>Reviews</h1>
+      {userReviews}
     </div>
   );
 };
