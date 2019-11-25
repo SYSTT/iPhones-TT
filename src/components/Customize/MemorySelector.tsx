6bc5b184237acb08b53b18ecadab69b2ae839dcd
jtaaa <joshua.allum@cognite.com>
2019-11-25 15:55:19 +0100

    Render correct trade price in customize page

--- src/components/Customize/MemorySelector.tsx
+++ src/components/Customize/MemorySelector.tsx
@@ -5,6 +5,7 @@ import { Configuration } from '../../modules/stock';
 import Price from '../Price';
 import { OptionList, OptionButton, Colors, dedup } from '../../utils';
 import { SelectorContainer } from './elements';
+import getCashDifference from '../../utils/getCashDifference';
 
 type Props = {
   memory?: number;
@@ -21,7 +22,32 @@ const MemorySelector: React.FC<Props> = ({
   disabled,
   tradeAmt = 0,
 }) => {
-  const renderOption = (optionMemory: number, optionPrice: number) => {
+  const renderTradePrice = (
+    orderItemPrice: number,
+    orderItemCost: number,
+    tradeItemPrice: number,
+  ) => {
+    const cashDifference = getCashDifference(orderItemCost, tradeItemPrice);
+    return (
+      <div style={{ display: 'flex' }}>
+        <div style={{ marginRight: '0.5em' }}>
+          You <strong>{cashDifference > 0 ? 'pay' : 'get'}</strong>
+        </div>
+        <Price
+          amt={orderItemPrice}
+          reduction={orderItemPrice - cashDifference}
+          block
+          abs
+        />
+      </div>
+    );
+  };
+
+  const renderOption = (
+    optionMemory: number,
+    optionPrice: number,
+    optionCost: number,
+  ) => {
     const outOfStock = !configs.find(
       ({ memory, stock }) => memory === optionMemory && stock > 0,
     );
@@ -37,11 +63,9 @@ const MemorySelector: React.FC<Props> = ({
           {optionMemory}
           <span style={{ fontSize: 14 }}>GB</span>
         </h2>
-        {outOfStock ? (
-          'Out of stock'
-        ) : (
-          <Price amt={optionPrice} reduction={tradeAmt} />
-        )}
+        {outOfStock
+          ? 'Out of stock'
+          : renderTradePrice(optionPrice, optionCost, tradeAmt)}
       </OptionButton>
     );
   };
@@ -51,7 +75,7 @@ const MemorySelector: React.FC<Props> = ({
       <h4 style={{ color: disabled ? Colors.Grey : 'initial' }}>Memory</h4>
       <OptionList>
         {dedup(configs, config => config.memory).map(config =>
-          renderOption(config.memory, config.price),
+          renderOption(config.memory, config.price, config.cost),
         )}
       </OptionList>
     </SelectorContainer>
