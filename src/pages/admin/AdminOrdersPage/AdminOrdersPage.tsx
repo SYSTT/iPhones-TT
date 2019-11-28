92e60c8ef9e14c02f805e5cb0cfc89843936b469
Joshua Allum <joshuatallum@gmail.com>
2019-11-28 19:06:49 +0100

    Update admin orders page to be much more usable (#79)

--- src/pages/admin/AdminOrdersPage/AdminOrdersPage.tsx
+++ src/pages/admin/AdminOrdersPage/AdminOrdersPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { User } from 'firebase';
-import { Spin, Select, Divider } from 'antd';
+import { Spin, Select, Collapse, Badge } from 'antd';
 
-import { Heading } from '../../../utils';
+import { Heading, Colors } from '../../../utils';
 import AuthCheck from '../../../components/AuthCheck/AuthCheck';
 import { Container, OrderViewContainer } from './element';
 import { useAuth } from '../../../modules/auth';
@@ -17,6 +17,8 @@ import {
 } from '../../../modules/orders';
 import { Profile } from '../../../components/forms/ProfileInfoForm';
 
+const { Panel } = Collapse;
+
 type OrderViewProps = {
   order: Order | TradeOrder;
   updateStatus: (newStatus: OrderStatus) => void;
@@ -29,27 +31,35 @@ const ProfileInfoView = ({
   phoneNumber,
 }: Omit<Profile, 'password'>) => {
   return (
-    <>
+    <div>
       <h3>User info</h3>
       <p>
         First name: {firstName}, Last name: {lastName}
       </p>
       <p>Email: {email}</p>
       <p>Phone number: {phoneNumber}</p>
-    </>
+    </div>
   );
 };
 
 const OrderView = ({ order, updateStatus }: OrderViewProps) => {
   return (
     <OrderViewContainer>
-      <h3>Order ID: {order.id}</h3>
-      <p>Model: {order.orderItem.model}</p>
-      <p>Memory: {order.orderItem.memory}</p>
-      <p>Color: {order.orderItem.color}</p>
-      <p>Quantity: {order.orderItem.quantity}</p>
-      <ProfileInfoView {...order.profileInfo} />
-      <Select value={order.status} onChange={updateStatus}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div style={{ marginRight: '2em' }}>
+          <h3>Order ID: {order.id}</h3>
+          <p>Model: {order.orderItem.model}</p>
+          <p>Memory: {order.orderItem.memory}</p>
+          <p>Color: {order.orderItem.color}</p>
+          <p>Quantity: {order.orderItem.quantity}</p>
+        </div>
+        <ProfileInfoView {...order.profileInfo} />
+      </div>
+      <Select
+        value={order.status}
+        onChange={updateStatus}
+        style={{ width: 100 }}
+      >
         {ORDER_STATUSES.map(status => (
           <Select.Option key={status} value={status}>
             {status}
@@ -88,87 +98,184 @@ type Props = {
 
 const AdminOrdersPage: React.FC<Props> = () => {
   const { user } = useAuth();
-  const { orders, updateOrderStatus } = useOrders();
-  const { tradeOrders, updateTradeOrderStatus } = useTradeOrders();
+  const { orders, updateOrderStatus, loading: loadingOrders } = useOrders();
+  const {
+    tradeOrders,
+    updateTradeOrderStatus,
+    loading: loadingTradeOrders,
+  } = useTradeOrders();
+
+  const renderPanelHeader = (header: string, count: number) => {
+    console.log({ header, count });
+    return (
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {header}{' '}
+        <Badge
+          count={count}
+          style={{
+            backgroundColor: Colors.Red,
+          }}
+        />
+      </div>
+    );
+  };
+
+  const renderQuickInfo = (
+    firstName: string,
+    lastName: string,
+    status: string,
+  ) => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div>
+          {firstName} {lastName}
+        </div>
+        <div>{status}</div>
+      </div>
+    );
+  };
+
+  const activeBuyOrders = orders.filter(
+    order => order.status !== 'cancelled' && order.status !== 'completed',
+  );
+  const activeTradeOrders = tradeOrders.filter(
+    order => order.status !== 'cancelled' && order.status !== 'completed',
+  );
+  const otherBuyOrders = orders.filter(
+    order => order.status === 'cancelled' || order.status === 'completed',
+  );
+  const otherTradeOrders = tradeOrders.filter(
+    order => order.status === 'cancelled' || order.status === 'completed',
+  );
 
   return (
     <AuthCheck user={user} fallback={<Spin />} requiredClaims={{ admin: true }}>
       <Container>
         <Heading>Orders</Heading>
-        <h1>Buy Orders</h1>
-        {orders
-          .filter(
-            order =>
-              order.status !== 'cancelled' && order.status !== 'completed',
-          )
-          .map(order => (
-            <>
-              <OrderView
-                key={order.id}
-                order={order}
-                updateStatus={(newStatus: OrderStatus) =>
-                  updateOrderStatus(order.id, newStatus)
-                }
-              />
-              <Divider />
-            </>
-          ))}
-        <h1>Trade Orders</h1>
-        {tradeOrders
-          .filter(
-            order =>
-              order.status !== 'cancelled' && order.status !== 'completed',
-          )
-          .map(order => (
-            <>
-              <OrderView
-                key={order.id}
-                order={order}
-                updateStatus={(newStatus: OrderStatus) =>
-                  updateTradeOrderStatus(order.id, newStatus)
-                }
-              />
-              <TradeItemView {...order.tradeItem} />
-              <Divider />
-            </>
-          ))}
-        <h1>Inactive Orders</h1>
-        {orders
-          .filter(
-            order =>
-              order.status === 'cancelled' || order.status === 'completed',
-          )
-          .map(order => (
-            <>
-              <OrderView
-                key={order.id}
-                order={order}
-                updateStatus={(newStatus: OrderStatus) =>
-                  updateTradeOrderStatus(order.id, newStatus)
-                }
-              />
-              <Divider />
-            </>
-          ))}
-        <h1>Inactive Trade Orders</h1>
-        {tradeOrders
-          .filter(
-            order =>
-              order.status === 'cancelled' || order.status === 'completed',
-          )
-          .map(order => (
-            <>
-              <OrderView
-                key={order.id}
-                order={order}
-                updateStatus={(newStatus: OrderStatus) =>
-                  updateTradeOrderStatus(order.id, newStatus)
-                }
-              />
-              <TradeItemView {...order.tradeItem} />
-              <Divider />
-            </>
-          ))}
+        <Collapse defaultActiveKey={['buy-orders', 'trade-orders']}>
+          {!loadingOrders && (
+            <Panel
+              header={renderPanelHeader('Buy Orders', activeBuyOrders.length)}
+              key="buy-orders"
+            >
+              {activeBuyOrders.map(order => (
+                <Collapse key={order.id}>
+                  <Panel
+                    header={renderQuickInfo(
+                      order.profileInfo.firstName,
+                      order.profileInfo.lastName,
+                      order.status,
+                    )}
+                    key="trade-orders"
+                  >
+                    <OrderView
+                      key={order.id}
+                      order={order}
+                      updateStatus={(newStatus: OrderStatus) =>
+                        updateOrderStatus(order.id, newStatus)
+                      }
+                    />
+                  </Panel>
+                </Collapse>
+              ))}
+            </Panel>
+          )}
+          {!loadingTradeOrders && (
+            <Panel
+              header={renderPanelHeader(
+                'Trade Orders',
+                activeTradeOrders.length,
+              )}
+              key="trade-orders"
+            >
+              {activeTradeOrders.map(order => (
+                <Collapse key={order.id}>
+                  <Panel
+                    header={renderQuickInfo(
+                      order.profileInfo.firstName,
+                      order.profileInfo.lastName,
+                      order.status,
+                    )}
+                    key="trade-orders"
+                  >
+                    <OrderView
+                      key={order.id}
+                      order={order}
+                      updateStatus={(newStatus: OrderStatus) =>
+                        updateTradeOrderStatus(order.id, newStatus)
+                      }
+                    />
+                    <TradeItemView {...order.tradeItem} />
+                  </Panel>
+                </Collapse>
+              ))}
+            </Panel>
+          )}
+          {!loadingOrders && (
+            <Panel
+              header={renderPanelHeader(
+                'Inactive Buy Orders',
+                otherBuyOrders.length,
+              )}
+              key="inactive-buy-orders"
+            >
+              {otherBuyOrders.map(order => (
+                <Collapse key={order.id}>
+                  <Panel
+                    header={renderQuickInfo(
+                      order.profileInfo.firstName,
+                      order.profileInfo.lastName,
+                      order.status,
+                    )}
+                    key="trade-orders"
+                  >
+                    <OrderView
+                      order={order}
+                      updateStatus={(newStatus: OrderStatus) =>
+                        updateTradeOrderStatus(order.id, newStatus)
+                      }
+                    />
+                  </Panel>
+                </Collapse>
+              ))}
+            </Panel>
+          )}
+          {!loadingTradeOrders && (
+            <Panel
+              header={renderPanelHeader(
+                'Inactive Trade Orders',
+                otherTradeOrders.length,
+              )}
+              key="inactive-trade-orders"
+            >
+              {otherTradeOrders.map(order => (
+                <Collapse key={order.id}>
+                  <Panel
+                    header={renderQuickInfo(
+                      order.profileInfo.firstName,
+                      order.profileInfo.lastName,
+                      order.status,
+                    )}
+                    key="trade-orders"
+                  >
+                    <OrderView
+                      order={order}
+                      updateStatus={(newStatus: OrderStatus) =>
+                        updateTradeOrderStatus(order.id, newStatus)
+                      }
+                    />
+                    <TradeItemView {...order.tradeItem} />
+                  </Panel>
+                </Collapse>
+              ))}
+            </Panel>
+          )}
+        </Collapse>
       </Container>
     </AuthCheck>
   );
